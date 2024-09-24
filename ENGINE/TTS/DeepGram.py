55d9fdc453c78124dad48a248b4cdc9e441398e4
Sreejan <121751299+SreejanPersonal@users.noreply.github.com>
2024-09-24 08:33:52 +0530

    Modified DeepGram TTS

--- ENGINE/TTS/DeepGram.py
+++ ENGINE/TTS/DeepGram.py
@@ -1,6 +1,7 @@
 import requests
 import base64
-from TOOLS.AUDIO import Interrupted_Playsound
+# from TOOLS.AUDIO import Interrupted_Playsound
+from playsound import playsound
 import os
 
 def speak(text: str, model: str = "aura-arcas-en", filename: str = "ASSETS/output_audio.mp3"):
@@ -21,16 +22,39 @@ def speak(text: str, model: str = "aura-arcas-en", filename: str = "ASSETS/outpu
     except: pass
     
     url = "https://deepgram.com/api/ttsAudioGeneration"
+
+    headers = {
+    "authority": "deepgram.com",
+    "method": "POST",
+    "path": "/api/ttsAudioGeneration",
+    "scheme": "https",
+    "accept": "*/*",
+    "accept-encoding": "gzip, deflate, br, zstd",
+    "accept-language": "en-US,en;q=0.9,hi;q=0.8",
+    "content-type": "application/json",
+    "origin": "https://deepgram.com",
+    "priority": "u=1, i",
+    "referer": "https://deepgram.com/",
+    "sec-ch-ua": '"Chromium";v="128", "Not;A=Brand";v="24", "Google Chrome";v="128"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+    "dnt": "1"
+}
     payload = {"text": text, "model": model}
        
 
-    response = requests.post(url, json=payload)
+    response = requests.post(url, headers=headers, json=payload)
     response.raise_for_status()  # Ensure the request was successful
 
     with open(filename, 'wb') as audio_file:
         audio_file.write(base64.b64decode(response.json()['data']))
     
-    Interrupted_Playsound(filename)
+    # Interrupted_Playsound.play_audio(filename)
+    playsound(filename)
     os.remove(filename)
 
 if __name__ == "__main__":
